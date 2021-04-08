8d0213ee2abe0467009b2cf7240eace24a05747f
eprikhodko <eprikhodkodev@gmail.com>
2021-04-08 17:02:52 +0300

    Add searchMovies function to the SearchMovies component

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,8 +1,26 @@
 import React from "react";
 
-export default function SearchMovies() {
+export default function SearchMovies(){
+    
+    const searchMovies = async (event) => {
+        event.preventDefault();
+        console.log("submitting");
+        
+        const query = "Galaxy Quest";
+        
+        const url = `https://api.tmdb.org/3/search/movie?api_key=496e7112180f6c0846886cf52e631b38&language=en-US&query=${query}&page=1&include_adult=false`;
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        } catch(err){
+            console.error(err);
+        }
+    }
+    
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
                 placeholder="i.e. Galaxy Quest"/>
