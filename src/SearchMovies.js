e67aef927afc2b0a0811c67e53ff4f4368eb142a
eprikhodko <eprikhodkodev@gmail.com>
2021-04-08 16:58:52 +0300

    Add basic styles to SearchMovies component

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -5,7 +5,7 @@ export default function SearchMovies() {
         <form className="form">
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park"/>
+                placeholder="i.e. Galaxy Quest"/>
             <button className="button" type="submit">Search</button>
         </form>
     )
