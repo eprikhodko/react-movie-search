de38d53a87d8545912215d512528cfead1f4ae66
eprikhodko <eprikhodkodev@gmail.com>
2021-04-08 17:58:08 +0300

    Add setMovies hook and map movies titles to the screen

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -2,9 +2,10 @@ import React, {useState} from "react";
 
 const MY_KEY = process.env.REACT_APP_API_KEY;
 
-export default function SearchMovies(){
+export default function SearchMovies() {
     
     const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (event) => {
         event.preventDefault();
@@ -14,24 +15,31 @@ export default function SearchMovies(){
         try {
             const response = await fetch(url);
             const data  = await response.json();
-            console.log(data);
+            setMovies(data.results);
+            console.log(movies)
         } catch(err){
             console.error(err);
         }
     }
     
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input 
-                className="input" 
-                type="text" 
-                name="query"
-                placeholder="i.e. Galaxy Quest" 
-                value={query} 
-                onChange={(event) => setQuery(event.target.value)}
-            />
-            <button className="button" type="submit">Search</button>
-        </form>
+        <div className="search-movies-component">
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input 
+                    className="input" 
+                    type="text" 
+                    name="query"
+                    placeholder="i.e. Galaxy Quest" 
+                    value={query} 
+                    onChange={(event) => setQuery(event.target.value)}
+                />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.map(movie => movie.title)}
+            </div>
+        </div>
+        
     )
 }
