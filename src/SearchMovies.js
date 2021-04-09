1bb9ab91d5d799e52478469219c74836c0118dcf
eprikhodko <eprikhodkodev@gmail.com>
2021-04-09 10:21:19 +0300

    Clear search field after submit

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -10,18 +10,20 @@ export default function SearchMovies() {
     const searchMovies = async (event) => {
         event.preventDefault();
         
+        
+
         const url = `https://api.tmdb.org/3/search/movie?api_key=${MY_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const response = await fetch(url);
             const data  = await response.json();
             setMovies(data.results);
-            console.log(movies)
         } catch(err){
             console.error(err);
         }
+        setQuery('')
     }
-    
+
     return (
         <div className="search-movies-component">
             <form className="form" onSubmit={searchMovies}>
@@ -30,7 +32,7 @@ export default function SearchMovies() {
                     className="input" 
                     type="text" 
                     name="query"
-                    placeholder="i.e. Galaxy Quest" 
+                    placeholder="i.e. Star Wars" 
                     value={query} 
                     onChange={(event) => setQuery(event.target.value)}
                 />
